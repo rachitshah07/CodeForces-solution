10fd5e0a4db19e4129e11551ed5ebfe96c39c5f3
rachitshah07 <72906451+rachitshah07@users.noreply.github.com>
2022-10-29 00:13:55 +0530

    Create 1029A_Many_Equal_Strings.cpp

---
+++ 1300 rating/1029A_Many_Equal_Strings.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+#define ll long long int
+#define MOD 1000000007
+#define sz(x) ((int)(x).size())
+#define pii pair<int, int>
+#define vi vector<int>
+#define fov(x,a) for(auto &x:a) cin>>x
+#define vii vector<vector<int>>
+#define vpp vector<pair<int,pii>>
+#define adj map<int,vector<int>> 
+#define vp vector<pii>
+#define vb vector<bool>
+#define ff first
+#define ss second
+#define mii map<ll, ll>
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rev(s) reverse(s.begin(),s.end())
+#define mini(a) *min_element(all(a))
+#define maxi(a) *max_element(all(a))
+#define lower(s) transform(s.begin(),s.end(), s.begin(), ::tolower)
+#define pb push_back
+#define INF 0x3f3f3f3f3f3f3f3f //to be approximately a 19-bit value.
+#define fastread()                                     \
+    (ios_base::sync_with_stdio(false), cin.tie(NULL)); \
+    cout.tie(NULL)
+
+
+int main()
+{
+   ll n,k,x=0;
+  cin>>n>>k;
+  string s;
+  cin>>s;
+  for(int i=0;i<n-1;i++){
+    if(s.substr(0,i+1)==s.substr(n-i-1)){
+      x=i+1;   
+    }
+  } 
+  cout<<s;  
+  k--;
+  while(k--) cout<<s.substr(x); 
+}
+
